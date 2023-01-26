c9dbe3b12dfa5c84f67ced260aa82f68141e42d2
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-01-26 14:39:25 -0300

    Atualizando Container pagina inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import Header from "../../components/Header";
+import { Container } from "./styles";
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Header title="Home" />
-    </div>
+    </Container>
   );
 }
